ba23b6b68bcdcfccb247ead0f7307866b928d4d9
DavidUrbaez <durbaezl@unal.edu.co>
2021-06-18 22:42:45 -0500

    vals

--- src/js/code.js
+++ src/js/code.js
@@ -1,15 +1,17 @@
 async function getData() {
+
+    const a = [1, -1.58, 1.01, -0.23];
     const b = [0.48, -1.43, 1.43, -0.48];
-    const a = [-1.58, 1.01 - 0.23];
 
-    const xs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-    const ys = [];
+    const N = 70;
+    const xs = new Array(N).fill(0);
+    const ys = new Array(N).fill(0);
 
+    xs[b.length - 1] = 1;
+    for (let index = 0; index < N; index++) {
+        ys[index + a.length] = math.dot(b.reverse(), xs.slice(index, b.length + index)) + math.dot(ys.slice(index, index + a.length - 1), a.slice(1).reverse().map(x => x * -1))
+    }
 
-    xs.forEach(row => {
-        ys.push(Math.sin(row));
-
-    })
 
     return {
         xs,
--- src/js/draw_chart.js
+++ src/js/draw_chart.js
@@ -1,57 +1,57 @@
-chartIt();
+ chartIt();
 
-async function chartIt() {
-    const data = await getData();
-    const ctx = document.getElementById('chart').getContext('2d');
+ async function chartIt() {
+     const data = await getData();
+     const ctx = document.getElementById('chart').getContext('2d');
 
-    var options = {
-        // responsive: true,
-        // maintainAspectRatio: false,
-        scales: {
+     var options = {
+         // responsive: true,
+         // maintainAspectRatio: false,
+         scales: {
 
-            x: {
-                grid: {
-                    color: 'white',
-                },
-                ticks: {
-                    color: 'white',
-                }
-            },
+             x: {
+                 grid: {
+                     color: 'white',
+                 },
+                 ticks: {
+                     color: 'white',
+                 }
+             },
 
-            y: {
-                grid: {
-                    color: 'white',
-                },
-                ticks: {
-                    color: 'white',
-                }
-            }
-        },
+             y: {
+                 grid: {
+                     color: 'white',
+                 },
+                 ticks: {
+                     color: 'white',
+                 }
+             }
+         },
 
-        plugins: {
-            legend: {
-                display: false
-            }
-        }
-    };
+         plugins: {
+             legend: {
+                 display: false
+             }
+         }
+     };
 
 
-    const myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: data.xs,
-            datasets: [{
-                label: 'Filter #1',
-                data: data.ys,
-                backgroundColor: 'rgba(0, 255, 25, 1)',
-                borderColor: 'rgba(0, 255, 25, 1)',
-                borderWidth: 1,
-                barThickness: 4,
-                // pointStyle: 'circle',
+     const myChart = new Chart(ctx, {
+         type: 'bar',
+         data: {
+             labels: data.xs,
+             datasets: [{
+                 label: 'Filter #1',
+                 data: data.ys,
+                 backgroundColor: 'rgba(0, 255, 25, 1)',
+                 borderColor: 'rgba(0, 255, 25, 1)',
+                 borderWidth: 1,
+                 barThickness: 4,
+                 // pointStyle: 'circle',
 
-            }]
-        },
-        options: options
-    });
+             }]
+         },
+         options: options
+     });
 
-}
+ }
